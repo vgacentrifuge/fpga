c49c874628debb06c31c09cdbcc3ce5d260f0a73
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-09-21 13:42:43 +0200

    fix: make cool

--- top.v
+++ top.v
@@ -2,7 +2,7 @@
 
 module top(
     input clk,
-    output reg [7:0] LED
+    output wire [7:0] LED
 );
     reg [31:0] counter_val;
     wire reset;
@@ -14,13 +14,6 @@ module top(
         .value(counter_val)
     );
 
-    always @(posedge clk) begin
-        if (counter_val == 32'd100000000) begin
-            reset <= 1'b1;
-        end else begin
-            reset <= 1'b0;
-        end
-    end
-
-    assign LED = counter_val[7:0];
+    assign reset = counter_val[31];
+    assign LED = counter_val[30:23];
 endmodule
