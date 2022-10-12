08f6716d43c67ed81aa274198bd1ffd15b2ed2ab
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-10-12 14:57:16 +0200

    feat: SPI slave + test

--- src/sim.v
+++ src/sim.v
@@ -1,8 +1,9 @@
+`include "src/counter.v"
+
 module sim(input clk,
            input reset,
            output reg [31:0] value);
     counter counter_1 (.clk (clk),
     .reset (reset),
-    .double (1'b0),
     .value (value));
 endmodule
--- src/spi/mcu_spi.v
+++ src/spi/mcu_spi.v
@@ -1,8 +1,8 @@
-module mcu_spi(
-    input clk,
+module mcu_spi(input clk,
+               input spi_clk,
+               input spi_ss,
+               input spi_mosi,
+               output spi_miso,
+) spi_slave spi_handler(.clk(clk), .spi_clk(spi_clk), .spi_ss(spi_ss), .spi_mosi(spi_mosi), .spi_miso(spi_miso), .byte_out(byte_out), .byte_ready(byte_ready));
     
-    input spi_clk,
-    input spi_ss,
-    input [0:0] spi_mosi,
-    output [0:0] spi_miso,
-    )
+endmodule
