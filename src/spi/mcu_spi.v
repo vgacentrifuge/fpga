6a8e1333b9bc9ba35992f42dd19424df12c1e82f
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-10-13 09:35:42 +0200

    feat: spi demo

--- src/spi/mcu_spi.v
+++
@@ -1,8 +0,0 @@
-module mcu_spi(input clk,
-               input spi_clk,
-               input spi_ss,
-               input spi_mosi,
-               output spi_miso,
-) spi_slave spi_handler(.clk(clk), .spi_clk(spi_clk), .spi_ss(spi_ss), .spi_mosi(spi_mosi), .spi_miso(spi_miso), .byte_out(byte_out), .byte_ready(byte_ready));
-    
-endmodule
