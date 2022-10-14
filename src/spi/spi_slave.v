e703a852ce4ea72c4b52f3c5492089313b840ae1
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-10-14 13:34:35 +0200

    fix: SPI slave reader

--- src/spi/spi_slave.v
+++ src/spi/spi_slave.v
@@ -3,30 +3,26 @@
 // Uses the internal clock and oversamples the SPI clock. This makes a lot of stuff
 // simpler to deal with (e. g. detecting start/end of messages)
 module spi_slave (input clk,
-                  input spi_clk,
-                  input spi_ss,
-                  input spi_mosi,
-                  output spi_miso,
+                  input hw_spi_clk,
+                  input hw_spi_ss,
+                  input hw_spi_mosi,
+                  output hw_spi_miso,
                   output [7:0] byte_out,
                   output byte_ready);
     // Store the state of the SPI clock in a register so we can check states to
     // determine if there is a falling/rising edge
-    reg [1:0] spi_clk_sr;
+    reg spi_clk_state;
+    reg [7:0] spi_clk_history;
     always @ (posedge clk)
-        spi_clk_sr <= {spi_clk_sr[0], spi_clk};
+        spi_clk_history <= {spi_clk_history[6:0], hw_spi_clk};
     
-    wire spi_clk_posedge = ~spi_clk_sr[1] && spi_clk_sr[0];
-    wire spi_clk_negedge = spi_clk_sr[1] && ~spi_clk_sr[0];
+    // Wait for signal to be stable for 8 (FPGA) clock cycles so any noise
+    // will not trigger a false positive for SPI clock cycle
+    wire spi_clk_posedge = ~spi_clk_state && spi_clk_history == 8'hFF;
+    wire spi_clk_negedge = spi_clk_state && spi_clk_history == 8'h00;
     
-    // Check if the slave select line just became high
-    // reg [1:0] spi_ss_sr;
-    // always @ (posedge clk)
-    //     spi_ss_sr <= {spi_ss_sr[0], spi_ss};
-    
-    wire spi_active = ~spi_ss;
-    // Slave select is active low, so signals are inverted here
-    // wire spi_ss_posedge = spi_ss_sr[1] & ~spi_ss_sr[0];
-    // wire spi_ss_negedge = ~spi_ss_sr[1] & spi_ss_sr[0];
+    // Remap for active high on the SPI slave select
+    wire spi_active = ~hw_spi_ss;
     
     reg [2:0] counter;
     reg [7:0] data_in;
@@ -37,23 +33,20 @@ module spi_slave (input clk,
         data_ready <= 0;
 
         if (~spi_active) begin
+            spi_clk_state <= 1'b0;
             counter <= 3'b000; // Reset counter when there is no message
-        end else begin
-            if (spi_clk_posedge)
-            begin
-                counter <= counter + 3'b001;
-                data_in <= {data_in[6:0], spi_mosi};
-                data_ready <= counter == 3'b111;
-            end
-            else
-            begin
-                if (spi_clk_negedge)
-                    data_out <= spi_active; // Just write back one to let MCU know we are reading
-            end
+        end else if (spi_clk_posedge) begin
+            spi_clk_state <= 1'b1;
+            counter <= counter + 3'b001;
+            data_in <= {data_in[6:0], hw_spi_mosi};
+            data_ready <= counter == 3'b111;
+        end else if (spi_clk_negedge) begin
+            data_out <= spi_active; // Just write back one to let MCU know we are reading
+            spi_clk_state <= 1'b0;
         end
     end
                 
     assign byte_ready = data_ready;
     assign byte_out   = data_in;
-    assign spi_miso   = data_out;
+    assign hw_spi_miso   = data_out;
 endmodule
