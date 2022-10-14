9bcd5f244a645b69b4abb62fc5dd327b078f49d7
Lab PC <erik98m@gmail.com>
2022-10-14 15:12:37 +0200

    Fixed SRAM write

--- src/sram/sram_interface.v
+++ src/sram/sram_interface.v
@@ -16,17 +16,9 @@ module sram_interface(input clk,
     // Constant signals (for our purposes)
     assign sram_advload     = 0;
     assign sram_chip_enable = 3'b010;
-    assign sram_clk_enable  = 0 ;
+    assign sram_clk_enable  = 0;
     assign sram_bw          = 2'b0;
 
-
-
-    //SRAM clk:
-    reg [1:0] counter_val;
-    assign sram_clk = counter_val[1];
-    always @(posedge clk) begin
-        counter_val <= counter_val + 1;
-    end
 
     // Latch register to store data till next sram negedge
     reg [19:0] addr_latch;
@@ -45,7 +37,7 @@ module sram_interface(input clk,
     reg [17:0] out_latch;
     
     // Latch data on sram_posedge
-    always @(posedge sram_clk) begin
+    always @(posedge clk) begin
         addr_latch <= addr;
         we_latch <= write_enable;
         data_latch <= data_in;
@@ -53,7 +45,7 @@ module sram_interface(input clk,
         out_latch <= sram_data;
     end
     // place onto sram on negedge, shift registers forward one tick
-    always @(negedge sram_clk) begin 
+    always @(negedge clk) begin 
         addr_hold <= addr_latch;
         data_wait_1 <= data_latch;
         data_wait_2 <= data_wait_1;
@@ -65,9 +57,10 @@ module sram_interface(input clk,
 
 
     
-    assign sram_write_enable = ~we_hold;
-    assign sram_oe           =  we_hold;
+    assign sram_write_enable = ~we_wait_1;
+    assign sram_oe           =  1'b0;
     assign sram_addr         = addr_hold;
+    assign sram_clk          = clk;
     
     // control inout:
     assign sram_data = (we_hold)?data_hold:18'bz;
