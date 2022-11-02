4eded79195321974d744587c9ee3e0e4dd9a5ccc
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-11-02 22:37:03 +0100

    feat: pipeline progress

--- src/pipeline/pipeline.v
+++ src/pipeline/pipeline.v
@@ -1,22 +1,102 @@
-module pipeline(// Background input recieved directly
-                input [15:0] BGRGB,
-                input [9:0] BGX,
-                input [8:0] BGY,
+module pipeline(
+                input clk,
 
-                input pixelEnable, // Whether we are blanking screen
+                // The input position of the current pixel
+                input [9:0] pixel_x,
+                input [9:0] pixel_y,
+
+                input [15:0] bg_pixel_in,
+                input output_enable, // Whether we are blanking screen
 
                 // Foreground coord sent to SRAM, pixel recieved
-                output [9:0] FGX,
-                output [8:0] FGY,
-                input  [15:0] FGRGB,
-
-                // Resulting pixel
-                output [15:0] pixelOut,
+                input  [15:0] fg_pixel_in,
+                input  fg_pixel_skip,
+                output [9:0] fg_pixel_request_x,
+                output [9:0] fg_pixel_request_x,
+                output fg_pixel_request_active,
+                
+                // Resulting pixel. Positions for sanity checks.
+                output reg [15:0] pixel_out,
+                output reg [9:0] pixel_x_out,
+                output reg [9:0] pixel_y_out,
 
                 // Control signals:
-                input chroma_key
-                // etc. etc.
+                input [1:0] ctrl_overlay_mode,
+                input [1:0] ctrl_fg_scale,
+                input [9:0] ctrl_fg_offset_x,
+                input [9:0] ctrl_fg_offset_y,
                 );
+    parameter FOREGROUND_FETCH_CYCLE_DELAY = 4; // The amount of cycles it takes for the foreground pixel value to be fetched
 
-    assign pixelOut = BGRGB;
+    // Buffers while we wait for foreground pixel
+    reg [(15 * FOREGROUND_FETCH_CYCLE_DELAY):0] bg_pixel_buffer;
+    reg [(9 * FOREGROUND_FETCH_CYCLE_DELAY):0] bg_pixel_x_buffer;
+    reg [(9 * FOREGROUND_FETCH_CYCLE_DELAY):0] bg_pixel_y_buffer;
+
+    // Handle foreground fetching
+    pipeline_foreground_scale fg_fetcher(
+        .clk(clk),
+        .ctrl_foreground_scale(ctrl_fg_scale),
+        .fg_offset_x(ctrl_fg_offset_x),
+        .fg_offset_y(ctrl_fg_offset_y),
+        .pixel_x(pixel_x),
+        .pixel_y(pixel_y),
+        .fg_pixel_x(fg_pixel_request_x),
+        .fg_pixel_y(fg_pixel_request_y),
+        .fg_active(fg_pixel_request_active)
+    );
+
+    // Assuming here that a new pixel is ready every clock cycle
+    always @(posedge clk)
+    begin
+        bg_pixel_buffer <= {bg_pixel_buffer[15 * (FOREGROUND_FETCH_CYCLE_DELAY - 1):0], bg_pixel_in};
+        bg_pixel_x_buffer <= {bg_pixel_x_buffer[9 * (FOREGROUND_FETCH_CYCLE_DELAY - 1):0], pixel_x};
+        bg_pixel_y_buffer <= {bg_pixel_y_buffer[9 * (FOREGROUND_FETCH_CYCLE_DELAY - 1):0], pixel_y};
+    end
+
+    // The pixel we are currently processing
+    wire [15:0] bg_pixel = bg_pixel_buffer[15 * FOREGROUND_FETCH_CYCLE_DELAY:15 * (FOREGROUND_FETCH_CYCLE_DELAY - 1)];
+    wire [9:0] bg_pixel_x = bg_pixel_x_buffer[9 * FOREGROUND_FETCH_CYCLE_DELAY:9 * (FOREGROUND_FETCH_CYCLE_DELAY - 1)];
+    wire [9:0] bg_pixel_y = bg_pixel_y_buffer[9 * FOREGROUND_FETCH_CYCLE_DELAY:9 * (FOREGROUND_FETCH_CYCLE_DELAY - 1)];
+
+    wire [15:0] chroma_keyed_result;
+    wire [15:0] overlayed_result;
+
+    // Chroma keying
+    pipeline_chroma_key chroma_keyer(
+        .clk(clk),
+        .enable(fg_pixel_in), // Requires a valid foreground pixel
+        .bg_pixel_in(bg_pixel),
+        .fg_pixel_in(fg_pixel_in),
+        .pixel_out(chroma_keyed_result)
+    );
+
+    // Overlaying
+    pipeline_foreground_overlay overlayer(
+        .clk(clk),
+        .enable(fg_pixel_in), // Requires a valid foreground pixel
+        .bg_pixel_in(bg_pixel),
+        .fg_pixel_in(fg_pixel_in),
+        .pixel_out(overlayed_result)
+    );
+
+    // Output
+    always @(posedge clk)
+    begin
+        pixel_x_out <= bg_pixel_x;
+        pixel_y_out <= bg_pixel_y;
+
+        if (~output_enable)
+        begin
+            pixel_out <= 16'b0;
+        end
+        else
+        begin
+            case (ctrl_overlay_mode)
+                2'b00: pixel_out <= chroma_keyed_result; // Chroma key
+                2'b01: pixel_out <= overlayed_result; // Overlay foreground
+                default: pixel_out <= bg_pixel;
+            endcase
+        end
+    end
 endmodule
---
+++ src/pipeline/pipeline_foreground_scale.v
@@ -0,0 +1,55 @@
+module pipeline_foreground_scale(
+    input clk,
+    input [1:0] ctrl_foreground_scale,
+    input [9:0] fg_offset_x,
+    input [9:0] fg_offset_y,
+    input [9:0] pixel_x,
+    input [9:0] pixel_y,
+
+    output reg [9:0] fg_pixel_x,
+    output reg [9:0] fg_pixel_y,
+    output reg fg_active
+);
+    parameter RESOLUTION_X = 640;
+    parameter RESOLUTION_Y = 480;
+
+    wire scale_full = ctrl_foreground_scale == 2'b11;
+    wire scale_half = ctrl_foreground_scale == 2'b10;
+    wire scale_quarter = ctrl_foreground_scale == 2'b01;
+
+    // TODO: Offset
+
+    always @ (posedge clk)
+    begin
+        fg_active <= 1'b0;
+
+        if (scale_full)
+        begin
+            fg_pixel_x <= pixel_x;
+            fg_pixel_y <= pixel_y;
+            fg_active <= 1'b1;
+        end
+        else if (scale_half)
+        begin
+            if (pixel_x >= RESOLUTION_X / 2 && pixel_y >= RESOLUTION_Y / 2) 
+            begin
+                fg_pixel_x <= (pixel_x - RESOLUTION_X / 2) << 1;
+                fg_pixel_y <= (pixel_y - RESOLUTION_Y / 2) << 1;
+                fg_active <= 1'b1;
+            end
+            else 
+                fg_active <= 1'b0;
+        end
+        else if (scale_quarter)
+        begin
+            if (pixel_x >= RESOLUTION_X / 4 && pixel_y >= RESOLUTION_Y / 4) 
+            begin
+                fg_pixel_x <= (pixel_x - RESOLUTION_X / 4) << 2;
+                fg_pixel_y <= (pixel_y - RESOLUTION_Y / 4) << 2;
+                fg_active <= 1'b1;
+            end
+            else
+                fg_active <= 1'b0;
+        end
+    end
+endmodule
