f6eae462ea683a13b5edc343779495fe0f3f01d9
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-11-03 11:06:41 +0100

    feat: clean up + sim start for demo top

--- src/vga/hvsync_generator.v
+++ src/vga/hvsync_generator.v
@@ -18,7 +18,7 @@ module hvsync_generator(input clk25,
     parameter H_BACK_PORCH  = 48;
     parameter V_BACK_PORCH  = 33;
     
-    assign in_display_area <= (pixel_position_x < X_RES) && (pixel_position_y < Y_RES);
+    assign in_display_area = (pixel_position_x < X_RES) && (pixel_position_y < Y_RES);
     
     assign hw_vga_h_sync = ~((pixel_position_x > (X_RES + H_FRONT_PORCH) && (pixel_position_x < (X_RES + H_SYNC + H_FRONT_PORCH))));
     assign hw_vga_v_sync = ~((pixel_position_y > (Y_RES + V_FRONT_PORCH) && (pixel_position_y < (Y_RES + V_SYNC + V_FRONT_PORCH))));
--- src/vga/vgademo_top.v
+++ src/vga/vgademo_top.v
@@ -1,24 +1,61 @@
-module vgademo_top(input clk,
-                   output reg [2:0] pixel,
-                   output hsync_out,
-                   output vsync_out);
-    wire inDisplayArea;
-    wire [9:0] CounterX;
+`include "src/vga/dac_handle.v"
+`include "src/vga/pixel_counter.v"
+
+module vgademo_top(input gclk100,
+                   input clk25,
+                   input [15:0] colour_bus_0,
+                   input dataclkin_0,
+                   input vsin_0,
+                   input hsin_0,
+                   input fidin_0,
+                   input [15:0] colour_bus_1,
+                   input dataclkin_1,
+                   input vsin_1,
+                   input hsin_1,
+                   input fidin_1,
+                   output [15:0] colour_bus_2,
+                   output hsync_out_0,
+                   output vsync_out_0,
+                   output dacclk_out_0,
+                   inout [35:0] sram_data_bus_0,
+                   output [19:0] sram_addr_bus_0,
+                   output sram_ce1p3_0,
+                   output sram_ce2_0,
+                   output sram_cen_0,
+                   output sram_we_0,
+                   output sram_clk_0,
+                   output sram_oe_0,
+                   output sram_adv_ld_0,
+                   inout [23:0] auxio_bus_0);
     
-    hvsync_generator hvsync(
-    .clk(clk),
-    .vga_h_sync(hsync_out),
-    .vga_v_sync(vsync_out),
-    .CounterX(CounterX),
-    .inDisplayArea(inDisplayArea)
+    wire in_display_area;
+    wire [9:0] pixel_x;
+    wire [9:0] pixel_y;
+    reg [15:0] pixel;
+    
+    pixel_counter counter(
+        .clk(clk25),
+        .counter_x(pixel_x),
+        .counter_y(pixel_y),
+        .in_display_area(in_display_area)
     );
     
-    always @(posedge clk)
-    begin
-        if (inDisplayArea)
-            pixel <= CounterX[9:6];
-        else // if it's not to display, go dark
-            pixel <= 3'b000;
-    end
+    dac_handle dac(
+        .clk25(clk25),
+        .pixel_in(pixel),
+        .pixel_x(pixel_x),
+        .pixel_y(pixel_y),
+        .hw_colour_bus(colour_bus_2),
+        .hw_hsync_out(hsync_out_0),
+        .hw_vsync_out(vsync_out_0),
+        .hw_dacclk_out(dacclk_out_0)
+    );
     
+    always @(posedge clk25)
+    begin
+        if (in_display_area)
+            pixel <= {pixel_x, 6'b0};
+        else
+            pixel <= 16'h0000;
+    end
 endmodule
