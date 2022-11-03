8dcab07a9e95ec0e6e2e4f33c969c348bd74c3fc
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-11-03 10:00:07 +0100

    chore: clean up hvsync gen

--- src/vga/hvsync_generator.v
+++ src/vga/hvsync_generator.v
@@ -6,9 +6,7 @@ module hvsync_generator(input clk25,
                         output hw_vga_h_sync,
                         output hw_vga_v_sync,
                         
-                        output reg in_display_area);
-    reg vga_HS, vga_VS;
-    
+                        output in_display_area);
     parameter X_RES = 640;
     parameter Y_RES = 480;
     
@@ -20,17 +18,8 @@ module hvsync_generator(input clk25,
     parameter H_BACK_PORCH  = 48;
     parameter V_BACK_PORCH  = 33;
     
-    always @(posedge clk25)
-    begin
-        vga_HS <= (pixel_position_x > (X_RES + H_FRONT_PORCH) && (pixel_position_x < (X_RES + H_SYNC + H_FRONT_PORCH)));
-        vga_VS <= (pixel_position_y > (Y_RES + V_FRONT_PORCH) && (pixel_position_y < (Y_RES + V_SYNC + V_FRONT_PORCH)));
-    end
+    assign in_display_area <= (pixel_position_x < X_RES) && (pixel_position_y < Y_RES);
     
-    always @(posedge clk25)
-    begin
-        in_display_area <= (pixel_position_x < X_RES) && (pixel_position_y < Y_RES);
-    end
-    
-    assign hw_vga_h_sync = ~vga_HS;
-    assign hw_vga_v_sync = ~vga_VS;
+    assign hw_vga_h_sync = ~((pixel_position_x > (X_RES + H_FRONT_PORCH) && (pixel_position_x < (X_RES + H_SYNC + H_FRONT_PORCH))));
+    assign hw_vga_v_sync = ~((pixel_position_y > (Y_RES + V_FRONT_PORCH) && (pixel_position_y < (Y_RES + V_SYNC + V_FRONT_PORCH))));
 endmodule
