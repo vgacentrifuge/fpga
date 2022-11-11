3d8cdf8af41d1118f4a32da4defa506c38404296
ERIK BIG BOI MARTINSEN <eirk98m@gmail.com>
2022-11-11 14:48:42 +0100

    use adc clock as output clock, and push everything through FIFOs in the main clock domain

--- src/vga/dac_handle.v
+++ src/vga/dac_handle.v
@@ -2,7 +2,7 @@
 //`include "src/vga/hvsync_generator.v"
 
 module dac_handle(
-    input clk25, // 25 MHz clock
+    input pixelclk,
 
     input [15:0] pixel_in,
     /*
@@ -15,6 +15,8 @@ module dac_handle(
      * The y position of the pixel. See more information about use above.
      */
     input [10:0] pixel_y,
+    
+    input has_pixel,
 
     output reg [15:0] hw_colour_bus,
     output hw_hsync_out,
@@ -27,29 +29,29 @@ module dac_handle(
     wire vsync_gen;
 
     signal_half_delay #(.SHIFT_AMOUNT(7)) hsync_delay(
-        .clk(clk25),
+        .clk(pixelclk),
         .signal_in(hsync_gen),
-        .signal_out(hw_hsync_out)
+        .signal_out(hw_hsync_out),
+        .clamp(~has_pixel)
     );
 
     signal_half_delay #(.SHIFT_AMOUNT(7)) vsync_delay(
-        .clk(clk25),
+        .clk(pixelclk),
         .signal_in(vsync_gen),
-        .signal_out(hw_vsync_out)
+        .signal_out(hw_vsync_out),
+        .clamp(~has_pixel)
     );
 
     hvsync_generator hvsync_generator(
-        .clk(clk25),
+        .clk(pixelclk),
         .pixel_position_x(pixel_x),
         .pixel_position_y(pixel_y),
-        .hw_vga_h_sync(hsync_gen),
-        .hw_vga_v_sync(vsync_gen),
+        .vga_h_sync(hsync_gen),
+        .vga_v_sync(vsync_gen),
         .in_display_area(in_display_area)
     );
 
     // Position pixel starting when clk goes high
-//    reg [10:0] x;
-//    reg [10:0] y;
 //    always @(posedge clk25) begin
 //        // Set to black if the pixel is not in the display area as a safeguard
 //        hw_colour_bus <= 16'b0;
@@ -68,12 +70,11 @@ module dac_handle(
 //            end
 //        end
 //    end
-    always @(posedge clk25) begin
+    always @(posedge pixelclk) begin
+        hw_colour_bus <= 16'b0;
         if (in_display_area) begin
-                hw_colour_bus <= pixel_in;
-        end else begin 
-            hw_colour_bus <= 16'b0;
+            hw_colour_bus <= pixel_in;
         end
     end
-    assign hw_dacclk_out = clk25;
+    assign hw_dacclk_out = pixelclk;
 endmodule
--- src/vga/hvsync_generator.v
+++ src/vga/hvsync_generator.v
@@ -1,10 +1,10 @@
 module hvsync_generator(input clk,
+
                         input [10:0] pixel_position_x,
                         input [10:0] pixel_position_y,
 
-                        output hw_vga_h_sync,
-    
-                        output hw_vga_v_sync,
+                        output vga_h_sync,
+                        output vga_v_sync,
                         
                         output in_display_area);
     parameter X_RES = 800;
@@ -19,7 +19,6 @@ module hvsync_generator(input clk,
     //parameter V_BACK_PORCH  = 33;
     
     assign in_display_area = (pixel_position_x < X_RES) && (pixel_position_y < Y_RES);
-    
-    assign hw_vga_h_sync = ((pixel_position_x >= (X_RES + H_FRONT_PORCH) && (pixel_position_x < (X_RES + H_SYNC + H_FRONT_PORCH))));
-    assign hw_vga_v_sync = ((pixel_position_y >= (Y_RES + V_FRONT_PORCH) && (pixel_position_y < (Y_RES + V_SYNC + V_FRONT_PORCH))));
+    assign vga_h_sync = ((pixel_position_x >= (X_RES + H_FRONT_PORCH) && (pixel_position_x < (X_RES + H_SYNC + H_FRONT_PORCH))));
+    assign vga_v_sync = ((pixel_position_y >= (Y_RES + V_FRONT_PORCH) && (pixel_position_y < (Y_RES + V_SYNC + V_FRONT_PORCH))));
 endmodule
