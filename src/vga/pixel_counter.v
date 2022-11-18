4a8e86272a8b620e26b328961b6b98d59ceceba4
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-11-18 16:42:28 +0100

    chore: try just outputting red

--- src/vga/pixel_counter.v
+++ src/vga/pixel_counter.v
@@ -1,17 +1,19 @@
-module pixel_counter(input clk,
-                     output reg [9:0] counter_x,
-                     output reg [9:0] counter_y,
+module pixel_counter #(
+    parameter PRECISION = 11
+) ( input clk,
+                     output reg [PRECISION - 1:0] counter_x,
+                     output reg [PRECISION - 1:0] counter_y,
                      output in_display_area);
-    parameter X_RES = 640;
-    parameter Y_RES = 480;
+    parameter X_RES = 800;
+    parameter Y_RES = 600;
     
-    parameter H_SYNC = 96;
-    parameter V_SYNC = 2;
+    parameter H_SYNC = 128;
+    parameter V_SYNC = 4;
     
-    parameter H_FRONT_PORCH = 16;
-    parameter V_FRONT_PORCH = 10;
-    parameter H_BACK_PORCH  = 48;
-    parameter V_BACK_PORCH  = 33;
+    parameter H_FRONT_PORCH = 40;
+    parameter V_FRONT_PORCH = 1;
+    parameter H_BACK_PORCH  = 88;
+    parameter V_BACK_PORCH  = 23;
     
     wire counter_x_max = (counter_x == (X_RES + H_SYNC + H_BACK_PORCH + H_FRONT_PORCH));
     wire counter_y_max = (counter_y == (Y_RES + V_SYNC + V_BACK_PORCH + V_FRONT_PORCH));
